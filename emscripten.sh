bbec40a5bd83c46e1d3310f9e79a5061e8fe199a
longwatermelon <longwatermelon1@gmail.com>
2023-03-04 19:04:26 -0700

    increase total memory for web

--- emscripten.sh
+++ emscripten.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-emcc -O2 -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sSDL2_IMAGE_FORMATS='["png"]' -sUSE_SDL_MIXER=2 --preload-file res -std=gnu17 src/*.c -o docs/index.html
+emcc -O2 -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sSDL2_IMAGE_FORMATS='["png"]' -sUSE_SDL_MIXER=2 -sTOTAL_MEMORY=1024MB --preload-file res -std=gnu17 src/*.c -o docs/index.html
